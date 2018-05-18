81462548003e4456da992b76641b4d22a411f791
Ondrej Ruttkay <ondrej@automattic.com>
2018-05-18 18:33:48 +0200

    Revert "Change test"
    This reverts commit 29952dacdceb62a155b5d3a3f3cc8c5e78866306.

--- WordPress/src/main/java/org/wordpress/android/ui/plugins/PluginListFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/plugins/PluginListFragment.java
@@ -51,7 +51,6 @@ public class PluginListFragment extends Fragment {
 
     private static final String ARG_LIST_TYPE = "list_type";
 
-
     protected PluginBrowserViewModel mViewModel;
 
     protected RecyclerView mRecycler;
